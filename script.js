b90ed90939384db248ab5bfd16e53e37f4d6dce3
Xandeajd <alexandre.delazeri@gmail.com>
2024-02-05 09:33:52 -0300

    Modificações

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('nome').textContent = "JEISON MARCELO MACHADO";
-document.getElementById('naturalidade').textContent = "Rio Negrinho/SC";
-document.getElementById('dataNascimento').textContent = "21/05/1981";
-document.getElementById('cpf').textContent = "006-357.389-02";
-document.getElementById('rg').textContent = "3264708";
-document.getElementById('dataAtual').textContent = "16 de novembro de 2023";
